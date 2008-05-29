6957d21c1fa438b0f267cfa3bb4578b8fa31ac5c
Mikael Vik <mikael.vik@gmail.com>
2008-05-30 00:20:39 +0200

    Added scripts to application.js for flex callback.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,9 +1,9 @@
 function eventIdCallback(id) 
 {
-	alert("Selected Event id: "+id);
+	//alert("Selected Event id: "+id);
 } 
 
 function attendantIdCallback(bekkId)
 {
-	alert("Selected Attendant bekkId: "+bekkId);
+	//alert("Selected Attendant bekkId: "+bekkId);
 }
