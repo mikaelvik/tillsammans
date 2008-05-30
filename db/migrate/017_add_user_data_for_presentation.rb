4cfb70596649b4dabceb9f49b442e9c57c5f5a39
Mikael Vik <mikael.vik@gmail.com>
2008-05-30 08:02:36 +0200

    Fixed encoding error.

--- db/migrate/017_add_user_data_for_presentation.rb
+++ db/migrate/017_add_user_data_for_presentation.rb
@@ -2,9 +2,9 @@ class AddUserDataForPresentation < ActiveRecord::Migration
   def self.up
     User.create!(:login => "Mikael Vik", :email => "mikael.vik@bekk.no",:bekk_id => 229, :location_id => 2, :location_text => "NAV NDU", :message => "ye ye ye")
     
-    User.create!(:login => "TorbjÃ¸rn Vatn", :email => "torbjorn.vatn@bekk.no",:bekk_id => 220, :location_id => 2, :location_text => "BEKK", :message => "Taxi on the ways")
+    User.create!(:login => "Torbjørn Vatn", :email => "torbjorn.vatn@bekk.no",:bekk_id => 220, :location_id => 2, :location_text => "BEKK", :message => "Taxi on the ways")
 
-    User.create!(:login => "Aslak HellesÃ¸y", :email => "aslak.hellesoy@bekk.no",:bekk_id => 247, :location_id => 1, :location_text => "BEKK", :message => "")
+    User.create!(:login => "Aslak Hellesøy", :email => "aslak.hellesoy@bekk.no",:bekk_id => 247, :location_id => 1, :location_text => "BEKK", :message => "")
   end
 
   def self.down
--- db/migrate/019_add_attendance_data_for_presentation.rb
+++ db/migrate/019_add_attendance_data_for_presentation.rb
@@ -1,13 +1,13 @@
 class AddAttendanceDataForPresentation < ActiveRecord::Migration
   def self.up
-    Attendance.create!(:user => User.find_by_login("TorbjÃ¸rn Vatn"), :event => Event.find_by_name("Fagdag"))
+    Attendance.create!(:user => User.find_by_login("Torbjørn Vatn"), :event => Event.find_by_name("Fagdag"))
 
-    Attendance.create!(:user => User.find_by_login("TorbjÃ¸rn Vatn"), :event => Event.find_by_name("Vinsmaking"))
+    Attendance.create!(:user => User.find_by_login("Torbjørn Vatn"), :event => Event.find_by_name("Vinsmaking"))
     Attendance.create!(:user => User.find_by_login("Mikael Vik"), :event => Event.find_by_name("Vinsmaking"))
-    Attendance.create!(:user => User.find_by_login("Aslak HellesÃ¸y"), :event => Event.find_by_name("Vinsmaking"))
+    Attendance.create!(:user => User.find_by_login("Aslak Hellesøy"), :event => Event.find_by_name("Vinsmaking"))
   end
 
   def self.down
-    Attendance.each { |attendance| attendance.destroy }
+    Attendance.find(:all).each { |attendance| attendance.destroy }
   end
 end
